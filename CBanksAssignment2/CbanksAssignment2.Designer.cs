0720e8cd64b301b6db51ae4d621eaf2982fef425
Cbanks4667 <guywithashirt@gmail.com>
2017-03-15 18:46:02 -0400

    initial commit

---
+++ CBanksAssignment2/CbanksAssignment2.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace CBanksAssignment2
+{
+    partial class CbanksAssignment2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pic00 = new System.Windows.Forms.PictureBox();
+            this.pic01 = new System.Windows.Forms.PictureBox();
+            this.pic02 = new System.Windows.Forms.PictureBox();
+            this.pic10 = new System.Windows.Forms.PictureBox();
+            this.pic20 = new System.Windows.Forms.PictureBox();
+            this.pic11 = new System.Windows.Forms.PictureBox();
+            this.pic12 = new System.Windows.Forms.PictureBox();
+            this.pic21 = new System.Windows.Forms.PictureBox();
+            this.pic22 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic22)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(167, 670);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(104, 96);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 0;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(290, 670);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(104, 96);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 0;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(412, 670);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(104, 96);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 0;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CBanksAssignment2.Properties.Resources.board;
+            this.pictureBox1.Location = new System.Drawing.Point(100, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 465);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pic00
+            // 
+            this.pic00.Location = new System.Drawing.Point(99, 12);
+            this.pic00.Name = "pic00";
+            this.pic00.Size = new System.Drawing.Size(134, 133);
+            this.pic00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic00.TabIndex = 2;
+            this.pic00.TabStop = false;
+            this.pic00.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic01
+            // 
+            this.pic01.Location = new System.Drawing.Point(266, 12);
+            this.pic01.Name = "pic01";
+            this.pic01.Size = new System.Drawing.Size(141, 133);
+            this.pic01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic01.TabIndex = 2;
+            this.pic01.TabStop = false;
+            this.pic01.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic02
+            // 
+            this.pic02.Location = new System.Drawing.Point(435, 12);
+            this.pic02.Name = "pic02";
+            this.pic02.Size = new System.Drawing.Size(141, 133);
+            this.pic02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic02.TabIndex = 2;
+            this.pic02.TabStop = false;
+            this.pic02.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic10
+            // 
+            this.pic10.Location = new System.Drawing.Point(100, 164);
+            this.pic10.Name = "pic10";
+            this.pic10.Size = new System.Drawing.Size(133, 147);
+            this.pic10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic10.TabIndex = 2;
+            this.pic10.TabStop = false;
+            this.pic10.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic20
+            // 
+            this.pic20.Location = new System.Drawing.Point(100, 340);
+            this.pic20.Name = "pic20";
+            this.pic20.Size = new System.Drawing.Size(133, 137);
+            this.pic20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic20.TabIndex = 2;
+            this.pic20.TabStop = false;
+            this.pic20.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic11
+            // 
+            this.pic11.Location = new System.Drawing.Point(266, 164);
+            this.pic11.Name = "pic11";
+            this.pic11.Size = new System.Drawing.Size(141, 147);
+            this.pic11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic11.TabIndex = 2;
+            this.pic11.TabStop = false;
+            this.pic11.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic12
+            // 
+            this.pic12.Location = new System.Drawing.Point(435, 164);
+            this.pic12.Name = "pic12";
+            this.pic12.Size = new System.Drawing.Size(141, 147);
+            this.pic12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic12.TabIndex = 2;
+            this.pic12.TabStop = false;
+            this.pic12.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic21
+            // 
+            this.pic21.Location = new System.Drawing.Point(266, 340);
+            this.pic21.Name = "pic21";
+            this.pic21.Size = new System.Drawing.Size(141, 137);
+            this.pic21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic21.TabIndex = 2;
+            this.pic21.TabStop = false;
+            this.pic21.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // pic22
+            // 
+            this.pic22.Location = new System.Drawing.Point(435, 340);
+            this.pic22.Name = "pic22";
+            this.pic22.Size = new System.Drawing.Size(141, 137);
+            this.pic22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic22.TabIndex = 2;
+            this.pic22.TabStop = false;
+            this.pic22.Click += new System.EventHandler(this.pic00_Click);
+            // 
+            // CbanksAssignment2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 565);
+            this.Controls.Add(this.pic20);
+            this.Controls.Add(this.pic10);
+            this.Controls.Add(this.pic22);
+            this.Controls.Add(this.pic12);
+            this.Controls.Add(this.pic02);
+            this.Controls.Add(this.pic21);
+            this.Controls.Add(this.pic11);
+            this.Controls.Add(this.pic01);
+            this.Controls.Add(this.pic00);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Name = "CbanksAssignment2";
+            this.Text = "Tic Tac Toe: by  Chris Banks";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic22)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic00;
+        private System.Windows.Forms.PictureBox pic01;
+        private System.Windows.Forms.PictureBox pic02;
+        private System.Windows.Forms.PictureBox pic10;
+        private System.Windows.Forms.PictureBox pic20;
+        private System.Windows.Forms.PictureBox pic11;
+        private System.Windows.Forms.PictureBox pic12;
+        private System.Windows.Forms.PictureBox pic21;
+        private System.Windows.Forms.PictureBox pic22;
+    }
+}
+
---
+++ CBanksAssignment2/CbanksAssignment2.cs
@@ -0,0 +1,284 @@
+﻿/*CBanksAssignment2
+ * 
+ * Tic Tac Toe Game
+ * 
+ * Revision History
+ *      Created: Chris Banks Sept 30th 2016
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CBanksAssignment2
+{
+ /// <summary>
+///Namespace for the form 
+/// </summary>
+    public partial class CbanksAssignment2 : Form
+    {
+    /// <summary>
+    /// List of available players
+    /// </summary>
+        public enum Player
+        {
+            X,
+            O
+        }
+        const int ROWS = 3;
+        const int COLUMNS = 3;
+        int[,] Board = new int[ROWS, COLUMNS];
+        int MoveNumber = 0;
+        Player currentPlayer = Player.X;
+        int playerNow = 0;
+       
+        /// <summary>
+        /// Initializes the form and Loads a NewGame
+        /// </summary>
+        public CbanksAssignment2()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+        /// <summary>
+        /// New Game method to reset all global fields and images
+        /// </summary>
+        private void NewGame()
+        {
+            
+
+            currentPlayer = Player.X;
+            MoveNumber = 0;
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int column = 0; column < COLUMNS; column++)
+                {
+                   
+                    Board[row, column] = -1;
+                    
+                }
+            }
+            pic00.Image = null;
+            pic01.Image = null;
+            pic02.Image = null;
+            pic10.Image = null;
+            pic11.Image = null;
+            pic12.Image = null;
+            pic20.Image = null;
+            pic21.Image = null;
+            pic22.Image = null;
+        }
+        /// <summary>
+        /// Click event handler for the PictureBoxes
+        ///Calls a method to load an image 
+        ///Calls a method to check the status of the board
+        ///Switches Player
+        /// </summary>
+        /// <param name="sender">Determines which button is pressed</param>
+        /// <param name="e"></param>
+        private void pic00_Click(object sender, EventArgs e)
+        {
+            var ImageButton = (PictureBox)sender;
+            string location = "";
+            int rows = 0;
+            int columns = 0;
+            location = ImageButton.Name.ToString();
+            rows = int.Parse(location[3].ToString());
+            columns = int.Parse(location[4].ToString());
+            bool imageLoaded = false;
+
+            imageLoaded = LoadImage(sender,rows,columns);
+            if (imageLoaded)
+            {
+                if (MoveNumber < 9)
+                {
+                    MoveNumber++;
+                    BoardState(rows, columns);
+                    currentPlayer = currentPlayer ==
+                        Player.X ? Player.O : Player.X;
+                } 
+                else
+                {
+                    MessageBox.Show(MoveNumber.ToString());
+                } 
+            }
+
+        }
+        /// <summary>
+        /// Loads the Image to the picture box and the player to the board array
+        /// </summary>
+        /// <param name="sender">Represents which picture box was selected
+            /// </param>
+        /// <param name="r">The Row Index of the picture selected</param>
+        /// <param name="c">The Column Index of the picture selected</param>
+        private bool LoadImage(object sender,int r, int c)
+        {
+            int rows = r;
+            int columns = c;
+            var ImageButton = (PictureBox)sender;
+            bool imageLoaded = true;
+            if (ImageButton.Image == null)
+            {
+                if (currentPlayer == 0)
+                {
+                    ImageButton.Image = Properties.Resources.x;
+                    Board[rows, columns] = 0;
+
+                }
+                else
+                {
+                    ImageButton.Image = Properties.Resources.o;
+                    Board[rows, columns] = 1;
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("You have already selected this image");
+                imageLoaded = false;
+            }
+            return imageLoaded;
+        }
+        /// <summary>
+        /// Checks The win conditions of the game
+        /// </summary>
+        /// <param name="r">The Row Index of the picture selected</param>
+        /// <param name="c">The Column Index of the picture selected</param>
+        public void BoardState(int r, int c)
+        {
+            int rows = r;
+            int columns = c;
+            int location = Math.Abs(rows - columns);
+            bool win = false;
+
+            if (location == 1)
+            {
+                win = CheckHorizontal(rows, columns);
+                if (win == false)
+                {
+                    win = CheckVerticle(rows, columns);
+                }
+            }
+            else
+            {
+                win = CheckDiaganol();
+                if (win==false)
+                {
+                    win = CheckHorizontal(rows, columns);
+                }
+                if(win==false)
+                {
+                    win = CheckVerticle(rows, columns);
+                }
+            }
+
+            if (win)
+            {
+                MessageBox.Show("The Winner is " + currentPlayer);
+                NewGame();
+                currentPlayer = currentPlayer == Player.X ? Player.O : Player.X;
+            }
+           else if(MoveNumber ==9)
+            {
+                MessageBox.Show("it is a draw!");
+                NewGame();
+                currentPlayer = currentPlayer == Player.X ? Player.O : Player.X;
+            }
+
+        }
+        /// <summary>
+        /// Checks to see if a win condition has been met
+            /// on the current row selected
+        /// </summary>
+        /// <param name="r">The Row Index of the picture selected</param>
+        /// <param name="c">The Column Index of the picture selected</param>
+        /// <returns>Win condition status</returns>
+        public bool CheckHorizontal(int r, int c)
+        {
+            bool win = true;
+            int row = r;
+            SetPlayer();
+            for ( int column = 0; column < COLUMNS; column++)
+            {
+                if(playerNow.Equals(Board[row,column]))
+                {
+                   
+                }
+                else
+                {
+                    win = false;
+                }
+            }
+            return win;
+        }
+        /// <summary>
+        /// Checks to see if a win condition has been met
+            /// on the current column selected
+        /// </summary>
+        /// <param name="r">The Row Index of the picture selected</param>
+        /// <param name="c">The Column Index of the picture selected</param>
+        /// <returns>Win Condition Status</returns>
+        public bool CheckVerticle(int r, int c)
+        {
+            bool win = true;
+            int column = c;
+
+            SetPlayer();
+            for (int row = 0; row < ROWS; row++)
+            {
+                if (playerNow.Equals(Board[row, column]))
+                {
+
+                }
+                else
+                {
+                    win = false;
+                }
+            }
+            return win;
+        }
+        /// <summary>
+        /// Sets an integer = to the current player index
+        /// </summary>
+        private void SetPlayer()
+        {
+            if (currentPlayer == Player.X)
+            {
+                playerNow = 0;
+                //MessageBox.Show(playerNow.ToString());
+            }
+            else if (currentPlayer == Player.O)
+            {
+                playerNow = 1;
+            }
+        }
+        /// <summary>
+        /// Checks to see if a win condition has been met on the diagonal  
+        /// </summary>
+        /// <returns>Win condition status</returns>
+        public bool CheckDiaganol()
+        {
+            SetPlayer();
+            int row = 1;
+            int column = 1;
+            bool win = false;
+
+            if (playerNow.Equals(Board[row, column]))
+            {
+                if (playerNow.Equals(Board[row - 1, column - 1]) &&
+                    (playerNow.Equals(Board[row + 1, column + 1])) ||
+                    (playerNow.Equals(Board[row - 1, column + 1])) &&
+                    (playerNow.Equals(Board[row + 1, column - 1])))
+                {
+                    win = true;
+                }
+            }
+            return win;
+        }
+    }
+}
